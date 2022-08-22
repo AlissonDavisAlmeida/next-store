33c86a283016339bd3ed75da11b5cc8af5240780
alisson <alissoneatena1@gmail.com>
2022-08-22 17:36:59 -0300

    merge updates and resolve conflicts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { DocumentContext, Html, Main, NextScript } from "next/document";
-<<<<<<< HEAD
 import Head from "next/head";
 
 
@@ -9,48 +8,27 @@ class MyDocument extends Document {
         const initialProps = await Document.getInitialProps(ctx);
 
         return { ...initialProps };
-=======
-
-
-class MyDocument extends Document {
-    static async getInitialProps(ctx: DocumentContext) {
-        const initialProps = await Document.getInitialProps(ctx);
-        return { ...initialProps }
->>>>>>> c2d10bd4e5d765190082ae9505a06d35757b6ba6
     }
 
     render() {
 
         return (
             <Html lang="pt-BR">
-<<<<<<< HEAD
                 <Head>
-=======
-                <head>
->>>>>>> c2d10bd4e5d765190082ae9505a06d35757b6ba6
                     <link
                         rel="stylesheet"
                         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
                     />
 
-<<<<<<< HEAD
                 </Head>
-=======
-                </head>
->>>>>>> c2d10bd4e5d765190082ae9505a06d35757b6ba6
                 <body>
                     <Main />
                     <NextScript />
                 </body>
             </Html>
         )
-<<<<<<< HEAD
 
     }
-=======
-    }
-
->>>>>>> c2d10bd4e5d765190082ae9505a06d35757b6ba6
 }
 
 export default MyDocument;
